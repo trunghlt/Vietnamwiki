7dcb0ab2da28ca84845ce6e00136176c0bd2591a
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2009-11-30 22:24:55 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos@6 46499d64-588c-4cb2-b276-432f5e434212

---
+++ trunk/top-admin/index.php
@@ -0,0 +1,24 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>VietnamWiki.net - A complete travel guide to Vietnam</title>
+
+<style type="text/css">
+.logo {
+	font-size: 2.6em;
+	font-weight: bold; 
+	font-family: Arial,Helvetica,serif;
+	color: #FFFFFF;
+	cursor:default;
+	font-family: Arial,Helvetica,serif;
+}
+</style>
+</head>
+<body>
+<div class="logo">VietnamWiki.net</div>
+<div>
+	<a href="/vhcs2/">VHCS2</a>
+	<a href="https://109.123.66.160:10000">Webmin</a>
+</div>
+</body>
+</html>
