c622ae375342eaedb06e2fa47514dbbb1121dfa4
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2009-12-02 21:26:08 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos@26 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/vnwk-admin/db.php
+++ trunk/vnwk-admin/db.php
@@ -2,9 +2,9 @@
 
 	function db_connect()
 	{
-      $db_connection = mysql_connect("localhost", "vietnamw_vietnam", "vietnam3004") 
+      $db_connection = mysql_connect("localhost", "root", "") 
             or die(mysql_error());
-      $db_select = mysql_select_db("vietnamw_vietnamwk") or die(mysql_error());            
+      $db_select = mysql_select_db("vietnamwiki") or die(mysql_error());            
 	}
 	
 	db_connect();
