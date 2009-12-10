f23e95517809b190a97149f5282fd9005242ce5d
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2009-12-10 03:42:16 +0000

    Them vao script trong cac file de yeu cau confirm lai. Them vao class free_result trong common.php de giai phong ket qua cu
    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@46 46499d64-588c-4cb2-b276-432f5e434212

--- vnwk-admin/menu.php
+++ vnwk-admin/menu.php
@@ -1,9 +1,10 @@
 <?php
 	include ("projax/projax.php");
-	include("init.php"); 	
+	include("db.php");
+	include("common.php"); 	
 
 	$projax= new Projax();
-		
+	$q = new db;	
 	if( isset($_GET["action"]) ) {
 		$action = $_GET["action"];
 		
@@ -13,7 +14,7 @@
 			$sql = "SELECT ord
 					FROM destinations
 					WHERE id = ".$id;
-			$q = new db;
+			
 			$q->query($sql);
 			$r = mysql_fetch_array($q->re);
 			$c = $r["ord"];
@@ -22,11 +23,13 @@
 			$sql = "UPDATE destinations
 					SET ord = ord - 1, old_ord = old_ord - 1
 					WHERE (ord > ".$c.")";
+			
 			$q->query($sql);
 			
 			//delete 					
 			$sql = "DELETE FROM destinations
 					WHERE id = " . $id;
+			
 			$q->query($sql);
 		}
 		
@@ -34,7 +37,7 @@
 			$name = $_POST["name"];
 			$sql = "SELECT MAX(ord) as 'maxord'
 					FROM destinations";
-			$q = new db;
+			
 			$q->query($sql);
 			if ($q->n > 0) {
 				$r = mysql_fetch_array($q->re);
@@ -46,12 +49,13 @@
 			$sql = "INSERT INTO destinations
 					(EngName, ord, old_ord)
 					VALUE ('".$name."','".$n."','".$n."')";
+			
 			$q->query($sql);		
 		}
 		
 		if ($action == "order") {
 			$order = $_POST['list'];
-			$q = new db;
+			
 			for ($i=0; $i < sizeof($order); $i++) {
 				$sql = "UPDATE destinations
 						SET ord = ".$i."
@@ -60,6 +64,7 @@
 			}		
 			$sql = "UPDATE destinations
 					SET old_ord = ord";
+			
 			$q->query($sql);
 		}
 	}
@@ -67,9 +72,17 @@
 	$sql = "SELECT * 
 			FROM destinations
 			ORDER BY ord";
-	$q = new db;
+	
 	$q->query($sql);	
 ?>
+<script type="text/javascript" language="javascript">
+	function confir(id){
+		if(window.confirm('Do You want delete this destination?'))
+		{
+			location.href = 'control.php?action=delete&id='+id;
+		}
+	}
+</script>
 <div id="dest">
 <h3>Destinations</h3>
 <ul id="list">
@@ -77,14 +90,14 @@
 	$i = 0;
 	while ($r = mysql_fetch_array($q->re)) {	?>
 		<li id="item_<?php echo $i?>">
-			<a href='control.php?action=delete&id=<?php echo $r["id"]?>'>x</a>
+			<a href='#' onclick='confir(<?php echo $r["id"]?>);'>x</a>
 			<?php echo $r["ord"]?>
 			<?php echo $r["EngName"]?>
 			<a href="index_menu.php?id=<?php echo $r["id"] ?>" target="index_menu">(i)</a>
 		</li>
 		<?php 
 		$i++;
-	} ?>
+	}?>
 </ul>
 <?php echo $projax->sortabe_element('list',array('update'=>'dest','complete'=>$projax->visual_effect('highlight','list') , 'url'=>'menu.php?action=order'));?>
 	<form action="control.php?action=add" method="post">
