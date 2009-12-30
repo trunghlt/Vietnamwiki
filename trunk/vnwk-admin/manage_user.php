3cbe0cc57b7568e1184765d14b58382ea9350711
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2009-12-30 06:35:06 +0000

    manage_user voi class la phan ket noi database,userframe la tao frame edit la chinh sua user va manage la phan ban va remove.Anh them vo dum e trong table users them vao 1 row la ban_user voi default= 0. A check dum e coi dc chua.
    git-svn-id: svn://109.123.66.160/home/vnwkrepos@57 46499d64-588c-4cb2-b276-432f5e434212

---
+++ trunk/vnwk-admin/manage_user.php
@@ -0,0 +1,108 @@
+<?
+session_start();
+//include file	
+	include("class_user.php");
+	include("common.php");
+	include("session.php");
+//check logged user 
+	process(session_id(), myip());
+	if (!logged_in()) header("location: login.php");
+?>
+<style>
+	a{
+		text-decoration:none;
+	}
+	a:visited{
+		color:#0000FF;
+	}
+	a:hover{
+		color:#00FF00;
+	}
+</style>
+<script language="javascript" type="text/javascript">
+	function confir(id,num_page,start){
+		if(window.confirm('Do You want delete this user?'))
+		{
+			location.href = "manage_user.php?id="+id+"&act=del&s="+start+"&page="+num_page;
+		}
+	}
+</script>
+<?
+	$str ='';
+	$u = new user;
+//--------delete user---------------//	
+	if(isset($_GET['act']) && $_GET['act']=='del')
+	{
+		$u->del_user($_GET['id']);
+	}
+	
+//---------ban user-----------------//
+	if(isset($_GET['act']) && $_GET['act']=='ban')
+	{
+		if($_GET['value']==1)
+		{
+			$arr_ban = array(ban_user=>0);
+			$u->edit_user($_GET['id'],$arr_ban);
+		}
+		else{
+			$arr_ban = array(ban_user=>1);
+			$u->edit_user($_GET['id'],$arr_ban);
+		}
+	}
+
+//----------------------------------//
+	$arr = $u->show_user($str);
+	
+	$row_per_page = 5;
+	$count_record = count($arr);
+
+//Pages
+		if( $count_record > $row_per_page)
+		{
+			$num_page = ceil($count_record/$row_per_page);
+		}
+		else{
+			$num_page = 1;
+		}
+
+//start position 
+	if(isset($_GET['s']))
+	{
+		$start = $_GET['s'];
+	}
+	else{
+		$start = 0;
+	}
+	
+//show user
+	$str = " limit $start,$row_per_page";
+	$arr = $u->show_user($str);
+	foreach($arr as $key=>$value){
+	   if($value!=NULL)
+	   {
+			echo "<div style='width:400px;'>";
+			echo "<a href='edit_user.php?id=$value[id]' target='user' style='width:50px;float:left;'>(Edit)</a>";
+			echo "<label style='width:160px;float:left;'>$value[username]</label>";
+			if($value[ban_user]==0)
+				echo "<a href='manage_user.php?id=$value[id]&act=ban&value=0&page=$num_page&s=$start' style='width:60px;float:left;'>(Allowed)</a>";
+			else
+				echo "<a href='manage_user.php?id=$value[id]&act=ban&value=1&page=$num_page&s=$start' style='width:60px;float:left;'>(Banned)</a>";
+			echo "<a href='#' onclick='confir($value[id],$num_page,$start);' style='width:10px;float:left;'>(x)</a>";
+			echo "</div>";
+			echo "<br />";
+		}
+	}
+echo "<br/>";	
+	//break page
+	if($num_page > 1){
+
+		$current_page = ($start/$row_per_page)+1;
+		for($i=1 ; $i <= $num_page; $i++)
+		{
+			if($current_page != $i)
+				echo "<a href='manage_user.php?s=".($i-1)*$row_per_page."&page=$num_page'> ".$i." </a>";
+			else
+				echo " ".$i." ";
+		}
+	}
+?>
---
+++ trunk/vnwk-admin/user_frame.php
@@ -0,0 +1,6 @@
+<html>
+<frameset cols="30%,40%">
+	<frame src="manage_user.php">
+	<frame src="" name="user">
+<noframes>Your's browser don't support Frame</noframes>
+</html>
