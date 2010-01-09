d9c280fdcd5ba317a06cb637cadb045b089c40a5
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-09 15:23:39 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@75 46499d64-588c-4cb2-b276-432f5e434212

---
+++ dialog.php
@@ -0,0 +1,21 @@
+<div id='confirm' title='Review'>Admin will review this post.Thanks</div>
+<script>
+jQuery(document).ready(function(){ 
+	post_review = jQuery("#confirm").dialog({
+		autoOpen: false,
+		width: '100',
+		height: '50',
+		modal: true,
+		resizable: false,
+		overlay: {
+			backgroundColor: '#000',
+			opacity: 0.5
+		},
+		buttons: {
+			'Submit': function() {
+				jQuery(this).dialog('close');
+			},
+		}
+	});
+});	
+</script>
