e98a266516c806834b303f5c62e35af157cce0dd
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-26 21:54:12 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@105 46499d64-588c-4cb2-b276-432f5e434212

---
+++ testmap.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<script type="text/javascript" src="js/lytebox/lytebox.js"></script>
+	<link rel="stylesheet" href="js/lytebox/lytebox.css" type="text/css" media="screen" />
+</head>
+<body>
+	<a href="map.php" rel="lyteframe" title="Goole map testing" rev="width: 800px; height: 500px; scrolling: no; ">Google Map</a>
+</body>
+</html>
