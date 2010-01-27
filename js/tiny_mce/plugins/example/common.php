f10f249adc9c12f49d5e99ae1ddbafc14c1c98f9
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-27 06:46:38 +0000

    Add button get link image in tinyMCE.
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@106 46499d64-588c-4cb2-b276-432f5e434212

---
+++ js/tiny_mce/plugins/example/common.php
@@ -0,0 +1,43 @@
+<?php
+	class db {
+		var $re;
+		var $sql;
+		var $n = 0;
+		
+		function query($s) {
+			$this->sql = $s;
+			free_result($this->re);//free old result
+			$this->re = dbquery($s);	
+			error_reporting(0);
+			$this->n = mysql_num_rows($this->re);
+			error_reporting(3);
+		}
+	}
+	
+	function dbquery($sql) {
+		$re = mysql_query($sql) or die(mysql_error());
+		return $re;
+	}
+	function free_result($re){
+		if($re)
+		{
+			@mysql_free_result($re);
+		}
+	}	
+	function logged_in() {
+		$ip = myip();
+		$sql = "SELECT *
+				FROM admin_sessions
+				WHERE ip = '".$ip."'";
+		$result = mysql_query($sql);
+		$row = mysql_fetch_array($result);
+		mysql_free_result($result);
+		return $row["logged_in"];
+	}
+
+	function myip() {
+		return $_SERVER['REMOTE_ADDR'];
+	}
+	
+
+?>
