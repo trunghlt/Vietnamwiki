2373f7ad4049ad812d115d12c7b92ab78433572e
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-27 14:52:54 +0000

    Fix setting, plugin link_url, img_url (get link)
    git-svn-id: svn://109.123.66.160/home/vnwkrepos@111 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/core/classes/Setting.php
+++ trunk/core/classes/Setting.php
@@ -1,9 +1,24 @@
 <?php
+/***************************************************************
+Class connect table setting in database
+***************************************************************/
 class Setting {
+//present property_name's value in setting
 	public $name;
-	public $value;
-	public $re;
 	
+//present property_value's value in setting	
+	public $value;
+	
+//take result in mysql_query	
+	public $re;
+
+/***************************************************************
+
+* query to get link in table setting
+
+* @return: return array with key =  property_name and value = property_value
+
+***************************************************************/	
 	public function query_get_link(){
 		$str = 'select * from setting';
 		$this->re = mysql_query($str);
@@ -13,12 +28,23 @@ class Setting {
 		$this->free();
 		return $r;
 	}
-	
+/***************************************************************
+
+* update table setting 
+
+* @param $str: which col will be update,
+ 
+* @param $where: statement with where
+
+***************************************************************/	
 	public function update($str,$where){
 		$s = "update setting set $str where $where";
 		mysql_query($s);
 	}
 
+/***************************************************************
+Free old result
+***************************************************************/
 	public function free(){
 		if($this->re){
 			@mysql_free_result($this->re);
