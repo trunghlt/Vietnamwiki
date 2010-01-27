5b7588a82ae66805a73e11b01993a385c6410b82
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-27 06:52:08 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos@107 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/js/tiny_mce/plugins/example/editor_plugin_src.js
+++ trunk/js/tiny_mce/plugins/example/editor_plugin_src.js
@@ -1,3 +1,40 @@
+<<<<<<< .mine
+(function() {
+
+	tinymce.create('tinymce.plugins.Link_urlPlugin', {
+
+		init : function(ed, url) {
+
+			ed.addCommand('mceLink_url', function() {
+				ed.windowManager.open({
+					file : url + '/dialog.php',
+					width : 420, 
+					height : 400, 
+					inline : 1
+				}, {
+					plugin_url : url, // Plugin absolute URL
+				});
+			});
+
+		ed.addButton('link_url', {
+				title : 'link_url.desc',
+				cmd : 'mceLink_url',
+				image : url + '/img/link_url.gif'
+			});
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'Link_url plugin',
+				author : 'Hieu',
+				version : "1.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('link_url', tinymce.plugins.Link_urlPlugin);
+=======
 (function() {
 
 	tinymce.create('tinymce.plugins.Img_urlPlugin', {
@@ -33,4 +70,5 @@
 
 	// Register plugin
 	tinymce.PluginManager.add('img_url', tinymce.plugins.Img_urlPlugin);
+>>>>>>> .r106
 })();
