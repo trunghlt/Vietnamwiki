ec89f7560033c21070f5fa0d7f9d9845930ca801
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-27 14:54:17 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos@113 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/js/tiny_mce/plugins/example/editor_plugin_src.js
+++ trunk/js/tiny_mce/plugins/example/editor_plugin_src.js
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 (function() {
 
 	tinymce.create('tinymce.plugins.Link_urlPlugin', {
@@ -17,9 +16,9 @@
 			});
 
 		ed.addButton('link_url', {
-				title : 'link_url.desc',
+				title : 'link_url',
 				cmd : 'mceLink_url',
-				image : url + '/img/link_url.gif'
+				image : url + '/img/plus.gif'
 			});
 		},
 
@@ -33,42 +32,5 @@
 	});
 
 	// Register plugin
-	tinymce.PluginManager.add('link_url', tinymce.plugins.Link_urlPlugin);
-=======
-(function() {
-
-	tinymce.create('tinymce.plugins.Img_urlPlugin', {
-
-		init : function(ed, url) {
-
-			ed.addCommand('mceImg_url', function() {
-				ed.windowManager.open({
-					file : url + '/dialog.php',
-					width : 1240, 
-					height : 900, 
-					inline : 1
-				}, {
-					plugin_url : url, // Plugin absolute URL
-				});
-			});
-
-		ed.addButton('img_url', {
-				title : 'img_url.desc',
-				cmd : 'mceImg_url',
-				image : url + '/img/img_url.gif'
-			});
-		},
-
-		getInfo : function() {
-			return {
-				longname : 'Img_url plugin',
-				author : 'Hieu',
-				version : "1.0"
-			};
-		}
-	});
-
-	// Register plugin
-	tinymce.PluginManager.add('img_url', tinymce.plugins.Img_urlPlugin);
->>>>>>> .r106
+	tinymce.PluginManager.add('link_url', tinymce.plugins.link_urlPlugin);
 })();
