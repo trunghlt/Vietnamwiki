601c398bcdd3bf7b1e9e86bed3a1c2a129c4bc1d
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-28 07:29:12 +0000

    fix dialog.php
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@117 46499d64-588c-4cb2-b276-432f5e434212

--- js/tiny_mce/plugins/example/dialog.php
+++ js/tiny_mce/plugins/example/dialog.php
@@ -9,29 +9,23 @@
 <body id='edition_manage'>
 <?php
 include("../../../../core/init.php");
-include("common.php");
 include("../../../../core/classes/filter.php");
-
-$q = new db;
+include("../../../../core/classes/ActiveRecord.php");
 
 if (isset($_GET["task"])) {
 	$task = $_GET["task"];
 	if (isset($_GET["index_id"])) $index_id = Filter::filterInput($_GET['index_id'],"/index.php",1);
 	if (isset($_GET["post_id"])) $edition_id = Filter::filterInput($_GET['post_id'],"/index.php",1);
-	
+	$q = new Active;
 	if (isset($_GET["id"])) {
 		$id = Filter::filterInput($_GET["id"],"index.php",1);
 		if ($id > 0) {
-			$sql = "SELECT *
-					FROM index_menu
-					WHERE dest_id = ".$id."
-					ORDER BY ord";
-			
-			$q->query($sql);
-			if ($q->n > 0) {
+			$q->orderby('ord');
+			$row = $q->select('','index_menu',"dest_id = $id");	
+			if ($q->get_num() > 0) {
 				echo "<p><label>Index menu </label>";
 				echo "<select name='index".$task."' id='index".$task."' onchange='javascript:update_index(1)'>";
-				while ($r = mysql_fetch_array($q->re)) {
+				foreach($row as $r) {
 					if (!isset($index_id)) {
 						$index_id = $r["id"];
 					}
@@ -48,24 +42,17 @@ if (isset($_GET["task"])) {
 
 	if (!isset($index_id)) $index_id = 0;
 	if ($task == "2") return;
-	$sql = "SELECT *
-			FROM posts
-			WHERE index_id = ".$index_id."
-			ORDER BY ord";
-	
-	$q->query($sql);
+	$q->orderby('ord');
+	$row = $q->select('','posts',"index_id = $index_id");	
 ?>
 	<form onsubmit="Link_urlDialog.insert();return false;" action="#">
 	<p>Selected Link: <select id="someval" name="someval" class="text" /></p>
 <?php
-	while ($r = mysql_fetch_array($q->re)) {
-		$sql = "SELECT post_subject,post_id
-				FROM posts_texts
-				WHERE post_id=".$r["post_id"];
-		$qq = new db;
-		$qq->query($sql);
-		$rr = mysql_fetch_array($qq->re);
-	echo "<option value='http://www.vietnamwiki.net/viewtopic.php?id=".$rr['post_id']."'>".$rr['post_subject']."</option>";
+$arr = array('post_subject','post_id');
+$qq = new Active;
+	foreach($row as $r) {
+		$rr = $qq->select($arr,'posts_texts',"post_id=$r[post_id]");
+	echo "<option value='http://www.vietnamwiki.net/viewtopic.php?id=".$rr[0]['post_id']."'>".$rr[0]['post_subject']."</option>";
 	}
 ?>
 </select>
@@ -88,12 +75,10 @@ if (isset($_GET["task"])) {
 <select name="dest1" id="dest1" onChange="javascript:update_dest(1)">
 	<option value="0">Unknown</option>
 	<?php
-	$sql= "SELECT * 
-		   FROM destinations 
-		   ORDER BY ord";
-	
-	$q->query($sql);
-	while ($r = mysql_fetch_array($q->re)) {
+	$q = new Active;
+	$q->orderby('ord');
+	$row = $q->select('','destinations','');	
+	foreach($row as $r) {
 		echo "<option value='".$r["id"]."'>".$r["EngName"]."</option>";
 	}
 	?>
