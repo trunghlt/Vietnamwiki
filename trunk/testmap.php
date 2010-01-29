cad0db4b6813dde982415a14f49565f29dd5dfb9
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-01-29 02:10:35 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos@125 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/testmap.php
+++ trunk/testmap.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
-	<script type="text/javascript" src="js/lytebox/lytebox.js"></script>
-	<link rel="stylesheet" href="js/lytebox/lytebox.css" type="text/css" media="screen" />
+<script type="text/javascript" src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js"></script>
+<script type="text/javascript" src="js/jquery/fancybox/jquery.fancybox-1.2.6.pack.js"></script>
+
+<link rel="stylesheet" href="js/jquery/fancybox/jquery.fancybox-1.2.6.css" type="text/css" media="screen"/>
 </head>
+
 <body>
-	<a href="map.php" rel="lyteframe" title="Goole map testing" rev="width: 800px; height: 500px; scrolling: no; ">Google Map</a>
+<a class="iframe" href="map.php">This goes to iframe</a>
+
+<script language="javascript">
+$(document).ready(function() {
+
+	/* This is basic - uses default settings */
+	
+	$("a#single_image").fancybox();
+	
+	/* Using custom settings */
+	
+	$("a#inline").fancybox({
+		'hideOnContentClick': true
+	});
+
+	$("a.group").fancybox({
+		'zoomSpeedIn':		300, 
+		'zoomSpeedOut':	300, 
+		'overlayShow':		false
+	});
+
+	$("a.iframe").fancybox({
+		'frameWidth': 	800,
+		'frameHeight': 	530
+	});
+});
+</script>	
 </body>
 </html>
