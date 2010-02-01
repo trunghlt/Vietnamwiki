82955790f472b8e8c37754048ab3943f7ce413ac
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-01 21:15:04 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@144 46499d64-588c-4cb2-b276-432f5e434212

--- forms/register_email.php
+++ forms/register_email.php
@@ -1,6 +1,6 @@
 <div id="FillEmailDialog" title="Add email">
 	<form id = "FillEmail">
-		<p>In order for us to notify you about changes in topics which you have created or editted, please register your Email address with the system.<br/>
+		<p>In order for us to notify you of changes in topics which you have created or editted, please register your Email address with the system.<br/>
 		 (<span style="color: red">Note:</span>We loathe spams, hence, we will never spam)</p>
 		<label for="email">Email: </label>
 		<input class="field" name="email" id="email" type="text" style="width:130px" /><br />
