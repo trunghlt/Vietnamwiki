8ccfd92471fb7be5f62dd7daf2cf1779d91ba589
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-02 16:40:59 +0000

    Add plugin price
    git-svn-id: svn://109.123.66.160/home/vnwkrepos@149 46499d64-588c-4cb2-b276-432f5e434212

---
+++ trunk/js/tiny_mce/plugins/price/editor_plugin_src.js
@@ -0,0 +1,42 @@
+(function() {
+
+	tinymce.create('tinymce.plugins.PricePlugin', {
+
+		init : function(ed, url) {
+
+			ed.addCommand('mcePrice', function() {
+				ed.windowManager.open({
+					file : url + '/dialog.htm',
+					width : 200, 
+					height : 100, 
+					inline : 1
+				}, {
+					plugin_url : url, // Plugin absolute URL
+				});
+			});
+
+		ed.addButton('price', {
+				title : 'price',
+				cmd : 'mcePrice',
+				image : url + '/img/dollar1.gif'
+			});
+		
+		ed.onNodeChange.add(function(ed, cm ,n) {
+				cm.setActive('price', ed.dom.getAttrib(n,'class').indexOf('price')==-1);
+			  	cm.setActive('price', ed.dom.getAttrib(n,'class').indexOf('price')!=-1);
+				});
+
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'Price plugin',
+				author : 'Hieu',
+				version : "1.0"
+			};
+		}
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('price', tinymce.plugins.PricePlugin);
+})();
