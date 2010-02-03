5440acc2e5d45d8a24d2ff8ccc1fd1de5f4167e1
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-03 15:03:40 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@154 46499d64-588c-4cb2-b276-432f5e434212

--- js/tiny_mce/plugins/price/js/price.js
+++ js/tiny_mce/plugins/price/js/price.js
@@ -1,6 +1,7 @@
 tinyMCEPopup.requireLangPack();
 var PriceDialog = {
-	init : function() {
+	init : function(ed) {
+		tinyMCEPopup.resizeToInnerSize();
 		var f = document.forms[0];
 		// Get the selected contents as text and place it in the input
 		
@@ -13,22 +14,10 @@ var PriceDialog = {
 	insert : function() {
 		// Insert the contents from the input into the document
 		var ed = tinyMCEPopup.editor, dom = ed.dom;
-	var tl;
-	switch(document.forms[0].price.value)
-	{
-		case '1': tl ="(<$10)";break;
-		case '2': tl ="($10-$50)";break;
-		case '3': tl ="($50-100$)";break;
-		case '4': tl ="($100-$200)";break;
-		case '5': tl ="($200-$500)";break;
-		case '6': tl ="($500-$1000)";break;
-		default: tl = "null"; break;
-	}
 		
 		tinyMCEPopup.execCommand('mceInsertContent', false, dom.createHTML('img', {
 							class:"price"+document.forms[0].price.value,
-							src:tinyMCEPopup.getWindowArg('plugin_url')+ "/img/dollar"+document.forms[0].price.value+".gif",
-							title:tl
+							src:tinyMCEPopup.getWindowArg('plugin_url')+ "/img/dollar"+document.forms[0].price.value+".gif"
 							}));
 		tinyMCEPopup.close();
 	}
