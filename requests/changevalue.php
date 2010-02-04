344e4fc5e43ed49648ed3ec2402f0f186f8558fc
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-04 11:43:51 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@165 46499d64-588c-4cb2-b276-432f5e434212

--- requests/changevalue.php
+++ requests/changevalue.php
@@ -1,23 +1,23 @@
 <?php
 $vl=isset($_POST['vl']);
-$id = $_POST['id'];
+$id = isset($_POST['id']);
 if($id && $vl){
 include('../core/init.php');
 include('../core/classes/Db.php');
-include('../core/classes/PostElement.php');
+include('../core/classes/Follow.php');
 include('../core/common.php');
 $vl = $_POST['vl'];
 $id = $_POST['id'];
 	if(isNumeric($id) && isNumeric($vl)){
-		$post = new PostElement;
+		$post = new Follow;
 		if($vl==1)
 		{
-			$post->update_follow('0',"post_id=$id");
+			$post->update_follow('0',"id=$id");
 			echo '0';
 		}
 		else
 		{
-			$post->update_follow('1',"post_id=$id");
+			$post->update_follow('1',"id=$id");
 			echo '1';
 		}
 	}
