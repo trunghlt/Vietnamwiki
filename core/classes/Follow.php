93474bf60a9c157ed85283010dd889df649d3503
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-07 10:01:05 +0000

    Edit profile, add Lastest Post in pofile
    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@185 46499d64-588c-4cb2-b276-432f5e434212

--- core/classes/Follow.php
+++ core/classes/Follow.php
@@ -7,7 +7,7 @@ class Follow {
 	public $post_id;
 	
 //present property_value's value in setting	
-	public $username;
+	public $user_id;
 	
 //take result in mysql_query	
 	public $re;
@@ -21,18 +21,92 @@ class Follow {
 * @param $where: statement with where
 
 ***************************************************************/	
-	function update_follow($value,$where){
+	public function update_follow($value,$id){
 		$q = new db;
-		if(is_array($where))
-		{
-			foreach($where as $key=>$value)
-				$s[] = "$key = $value";
-				$where = implode(' and ',$s);
-				$where = "where $where";
-		} 
-		else
-			$where = "where $where";
-		$q->query("update follow set follow=$value $where");
+		$q->query(" UPDATE follow
+			
+					SET value=1
+			
+					WHERE id={$id}");
+	}
+/***************************************************************
+
+* query table follow 
+
+* @param $user_id: check user_id,
+ 
+* @param $post_id: and check post_id
+
+* return num row
+
+***************************************************************/	
+	static public function exist($user_id,$post_id){
+			$q = new Db;
+			$q->query("SELECT * FROM follow
+			
+			WHERE user_id={$user_id} AND post_id={$post_id}");
+			
+			return ($q->n);
+	}
+/***************************************************************
+
+* set value=1 in table follow 
+
+* @param $user_id: check user_id,
+ 
+* @param $post_id: and check post_id
+
+***************************************************************/	
+	static function set($user_id, $post_id) {
+			$q = new Db;
+		if (self::exist($user_id, $post_id) == 0) {
+		
+			Db::squery("INSERT INTO follow
+			
+			(user_id, post_id, value)
+			
+			VALUES({$user_id}, {$post_id}, 1)");
+		
+		}
+		
+		else {
+		
+			Db::squery(" UPDATE follow
+			
+			SET value=1
+			
+			WHERE user_id={$user_id} AND post_id={$post_id}");
+		}
+	}
+/***************************************************************
+
+* set value=1 in table follow 
+
+* @param $user_id: check user_id,
+ 
+* @param $post_id: and check post_id
+
+***************************************************************/	
+	static function un_set($user_id, $post_id) {
+			$q = new Db;
+		if (self::exist($user_id, $post_id) == 0) {
+		
+			Db::squery("INSERT INTO follow
+			
+			(user_id, post_id, value)
+			
+			VALUES({$user_id}, {$post_id}, 0)");
+		
+		}
+		
+		else {
+		
+			Db::squery(" UPDATE follow
+			
+			SET value=0
+			
+			WHERE user_id={$user_id} AND post_id={$post_id}");
+		}
 	}
 /***************************************************************
 Free old result
