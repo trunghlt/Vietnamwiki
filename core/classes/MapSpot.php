a8b6a994bf186c4a40e5b7d9d35fc82afcfc2dd4
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-07 01:18:15 +0000

    testing
    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@172 46499d64-588c-4cb2-b276-432f5e434212

--- core/classes/MapSpot.php
+++ core/classes/MapSpot.php
@@ -53,6 +53,7 @@ class MapSpot {
 	public static function getMapSpots($HTMLContent) {
 		$html = str_get_html($HTMLContent);
 		$mapDivList = $html->find(".map");
+		echo $mapDivList;
 		$mapSpotList = array();
 		foreach ($mapDivList as $mapDiv) {
 			$divContent = $mapDiv->innertext;
