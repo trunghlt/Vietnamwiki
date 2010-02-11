344a4f8ba15adf7560e2e2f47179920de18fa90b
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-02-11 03:09:55 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@197 46499d64-588c-4cb2-b276-432f5e434212

--- get.php
+++ get.php
@@ -5,12 +5,29 @@ if(isset($_POST['ok'])){
 	include('core/classes.php');
 	include('core/session.php');
 	include('core/filters.php');
-	$re = mysql_query('select post_username,post_id from posts ');
+	$re = mysql_query('select user_id,post_id from editions where checked=0 and post_id!=0');
 	while($row = mysql_fetch_assoc($re))
 	{
-		$str = "insert into follow(username,post_id) values('$row[post_username]',$row[post_id])";
-		mysql_query($str);
+		//$re2 = mysql_query("select post_id, user_id from follow where post_id=$row[post_id] and user_id=$row[user_id]");
+		$re2 = mysql_query("select user_id,post_id from follow where user_id=$row[user_id] and post_id=$row[post_id]");
+		if(mysql_num_rows($re2) == 0){
+				if(mysql_num_rows($re2) == 0){
+					$str = "insert into follow(user_id,post_id,value) values($row[user_id],$row[post_id],1)";
+					mysql_query($str);
+				}
+			}
 	}
+	
+	//$re = mysql_query('select id from users where level=1');
+	//while($row = mysql_fetch_assoc($re))
+	//{
+		//		$re2 = mysql_query("select post_id from posts");
+
+		//		while($row2 = mysql_fetch_array($re2)){
+		//			$str = "insert into follow(user_id,post_id,value) values($row[id],$row2[post_id],1)";
+		//			mysql_query($str);
+		//		}
+	//}
 }	
 ?>
 <form action="get.php" method="post">
