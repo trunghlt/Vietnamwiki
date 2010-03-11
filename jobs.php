baf471e6e4011da1c0c4c97e09207433e36770f7
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-03-11 15:48:18 +0000

    Job page
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@210 46499d64-588c-4cb2-b276-432f5e434212

--- jobs.php
+++ jobs.php
@@ -24,37 +24,38 @@ include('destination.php');
 		</ul>
 		
 		<h2>Part-time travel writer</h2>
-		<p>The role is to do research and to write articles about traveling information to Vietnam for the website in English. We do not require much from applicants. If you feel you have these following abilities and characters, please send your CV (in English) to careers@vietnamwiki.net:</p>
-		<ul>
-		<li><strong>Mandatory</strong>: Very good at writing and reading English.</li>
-		<li>Loving traveling, loving Vietnam, general knowledge of internet and enjoying working with a laptop and internet are advantages.</li>
-		<li>Having a computer and internet at home.</li>
-		<li>The ability to work by yourself.</li>
-		</ul>
-		<p>You will work 20 hours/week remotely with your own computer and internet.</p>
-		
-		<p><span style="color: #FF0000;">Note:</span> Students in all academic years are welcome to apply.</p>
+        <img src="http://www.clker.com/cliparts/2/6/8/4/122361306154509854shokunin_backpacker_on_a_phone.svg.hi.png" style="width: 140px; height: auto; float: left; margin-right: 50px;"/>
+        <p>The role is to do research and to write articles about traveling information to Vietnam for the website in English. We do not require much from applicants. If you feel you have these following abilities and characters, please send your CV (in English) to careers@vietnamwiki.net:</p>
+        <ul>
+        <li><strong>Mandatory</strong>: Very good at writing and reading English.</li>
+        <li>Loving traveling, loving Vietnam, general knowledge of internet and enjoying working with a laptop and internet are advantages.</li>
+        <li>Having a computer and internet at home.</li>
+        <li>The ability to work by yourself.</li>
+        </ul>
+        <p>You will work 20 hours/week remotely with your own computer and internet.</p>
 
-		<p><strong>Important:</strong> Please indicate clearly what relevant modules you have taken in your high school, university and their marks (if available).</p>
+        <p><span style="color: #FF0000;">Note:</span> Students in all academic years are welcome to apply.</p>
 
-		<p>Once you send your CV (or any queries) to us, we will try our best to reply in 2 working days.</p>
+        <p><strong>Important:</strong> Please indicate clearly what relevant modules you have taken in your high school, university and their marks (if available).</p>
 
-		<h2>Part-time web developer</h2>
-		<p>The role is to develop the website using PHP, MySQL, Javascript and JQuery. We do not require much from applicants. If you feel you have these following abilities and characters, please send your CV (in English) to careers@vietnamwiki.net:</p>
-		<ul>
-		<li><strong>Mandatory</strong>: at least 1 year experience with PHP or Javascript (this could be self-taught experience).</li>
-		<li>Willing to learn new knowledge.</li>
-		<li>Having a computer and internet at home.</li>
-		<li>The ability to work by yourself.</li>
-		</ul>
-		<p>You will work 20 hours/week remotely with your own computer and internet.</p>
-		
-		<p><span style="color: #FF0000;">Note:</span> Students in all academic years are welcome to apply.</p>
+        <p>Once you send your CV (or any queries) to us, we will try our best to reply in 2 working days.</p>
 
-		<p><strong>Important:</strong> Please indicate clearly what relevant modules you have taken in your high school, university and their marks (if available).</p>
+	    <h2>Part-time web developer</h2>
+        <img src="http://www.wanderingsandmusings.com/wp-content/uploads/2008/08/ninja.jpg" style="width: 200px; height: auto; float: left;"/>
+	    <p>The role is to develop the website using PHP, MySQL, Javascript and JQuery. We do not require much from applicants. If you feel you have these following abilities and characters, please send your CV (in English) to careers@vietnamwiki.net:</p>
+	    <ul>
+	    <li><strong>Mandatory</strong>: at least 1 year experience with PHP or Javascript (this could be self-taught experience).</li>
+	    <li>Willing to learn new knowledge.</li>
+	    <li>Having a computer and internet at home.</li>
+	    <li>The ability to work by yourself.</li>
+	    </ul>
+	    <p>You will work 20 hours/week remotely with your own computer and internet.</p>
+	
+	    <p><span style="color: #FF0000;">Note:</span> Students in all academic years are welcome to apply.</p>
 
-		<p>Once you send your CV (or any queries) to us, we will try our best to reply in 2 working days.</p>
-		
+	    <p><strong>Important:</strong> Please indicate clearly what relevant modules you have taken in your high school, university and their marks (if available).</p>
+
+	    <p>Once you send your CV (or any queries) to us, we will try our best to reply in 2 working days.</p>
 	</td><tr></tbody></table>
 </div>
 </td>
