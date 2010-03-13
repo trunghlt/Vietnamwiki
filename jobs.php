d6db620632093f0a42f21ff9c4380adf87edb399
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-03-13 10:41:10 +0000

    Update email address for job page
    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@214 46499d64-588c-4cb2-b276-432f5e434212

--- jobs.php
+++ jobs.php
@@ -25,7 +25,7 @@ include('destination.php');
 		
 		<h2>Part-time travel writer</h2>
         <img src="http://www.clker.com/cliparts/2/6/8/4/122361306154509854shokunin_backpacker_on_a_phone.svg.hi.png" style="width: 140px; height: auto; float: left; margin-right: 50px;"/>
-        <p>The role is to do research and to write articles about traveling information to Vietnam for the website in English. We do not require much from applicants. If you feel you have these following abilities and characteristics, please send your CV (in English) to vietnamwiki@googlemail.com:</p>
+        <p>The role is to do research and to write articles about traveling information to Vietnam for the website in English. We do not require much from applicants. If you feel you have these following abilities and characteristics, please send your CV (in English) to vietnamwiki@gmail.com:</p>
         <ul>
         <li><strong>Mandatory</strong>: Very good at writing and reading English.</li>
         <li>Loving traveling, loving Vietnam, general knowledge of internet and enjoying working with a laptop and internet are advantages.</li>
@@ -42,7 +42,7 @@ include('destination.php');
 
 	    <h2>Part-time web developer</h2>
         <img src="http://www.wanderingsandmusings.com/wp-content/uploads/2008/08/ninja.jpg" style="width: 200px; height: auto; float: left;"/>
-	    <p>The role is to develop the website using PHP, MySQL, Javascript and JQuery. We do not require much from applicants. If you feel you have these following abilities and characteristics, please send your CV (in English) to vietnamwiki@googlemail.com:</p>
+	    <p>The role is to develop the website using PHP, MySQL, Javascript and JQuery. We do not require much from applicants. If you feel you have these following abilities and characteristics, please send your CV (in English) to vietnamwiki@gmail.com:</p>
 	    <ul>
 	    <li><strong>Mandatory</strong>: at least 1 year experience with PHP or Javascript (this could be self-taught experience).</li>
 	    <li>Willing to learn new knowledge.</li>
