272d3cea10cefa24121d6abf39495566b989bdb4
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-03-30 17:38:30 +0000

    Fixed avatar in profile
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@244 46499d64-588c-4cb2-b276-432f5e434212

--- avatar.php
+++ avatar.php
@@ -1,3 +1,4 @@
+<<<<<<< .mine
 <?php
 	$sql = "SELECT avatar
 			FROM users
@@ -14,4 +15,21 @@
 //$pAvatar = new TalkPHP_Gravatar();
 //$pAvatar->setEmail($row['email'])->setSize(80)->setRatingAsPG();
 ?>
-<img width="100px" height="100px" alt="avatar" src="images/avatars/<?php echo $fn//$pAvatar->getAvatar()?>" />
+<img width="100px" height="100px" alt="avatar" src="images/avatars/<?php echo $fn//$pAvatar->getAvatar()?>" />=======
+<?php
+	$sql = "SELECT avatar
+			FROM users
+			WHERE id='".$user_info["id"]."'";
+	$q->query($sql);
+	$row = mysql_fetch_array($q->re);
+	$ava = $row["avatar"];
+	if (isset($ava) && $ava!='') { 
+		$fn = $ava;
+	}
+	else {
+		$fn = "unknown.jpg";
+	}
+//$pAvatar = new TalkPHP_Gravatar();
+//$pAvatar->setEmail($row['email'])->setSize(80)->setRatingAsPG();
+?>
+<img width="100px" height="100px" alt="avatar" src="images/avatars/<?php echo $fn//$pAvatar->getAvatar()?>" />>>>>>>> .r243
