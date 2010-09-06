6c2f48cc8a028f39ec7c090f108112b923eed642
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-09-06 08:31:28 +0000

    finish Question answer
    git-svn-id: svn://109.123.66.160/home/vnwkrepos@287 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/listquestion.php
+++ trunk/listquestion.php
@@ -5,16 +5,36 @@
 </div>
 <script>
 jQuery(document).ready(function(){
-          load_qanda(0);
+         load_qanda(0);
 });
     function load_qanda(id){
- 	jQuery.post("requests/QandA.php", {start:id},
-				function(response) {
-                                      jQuery('#view_qanda').html(response);
-				});
+        jQuery('#questionDialog').remove();
+        jQuery('#answerDialog').remove();
+        jQuery("#Emailquestion1").remove();
+        jQuery("#Emailanswer1").remove();
+        <?php
+            if(isset($post_id) && $post_id>0){
+        ?>
+                jQuery.post("requests/QandA.php", {start:id,post_id:<?php echo $post_id;?>,index_id:<?php echo $index_id;?>,destination:<?php echo $destination;?>},
+                                        function(response) {
+                                                jQuery('#view_qanda').html(response);
+                                        });
+          <?php
+            }
+            else if(isset($index_id) && $index_id>0){
+                $post_q = new PostElement;
+                $row_q = $post_q->query("",$index_id);
+          ?>
+                jQuery.post("requests/QandA.php", {start:id,post_id:<?php echo $row_q[0]['post_id'];?>,index_id:<?php echo $index_id;?>,destination:<?php echo $dest_id;?>},
+                                        function(response) {
+                                                jQuery('#view_qanda').html(response);
+                                        });
+          <?php
+            }
+        ?>
     }
     function question(){
- 	jQuery('#questionDialog').css('visibility','visible').dialog('open');
+       jQuery('#questionDialog').css('visibility','visible').dialog('open');
     }
     function answer(id){
  	jQuery('#answerDialog').css('visibility','visible').dialog('open');
