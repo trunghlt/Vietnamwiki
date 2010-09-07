513c1087157744df99665845bf3d64860df7bc19
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-09-07 13:48:13 +0000

    Newest codes..
    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@288 46499d64-588c-4cb2-b276-432f5e434212

--- viewtopic.php
+++ viewtopic.php
@@ -1,26 +1,26 @@
 <?php
-	include('core/common.php');
-	include('core/init.php');
-	include('core/classes.php');
-	include('core/session.php');
-	//include("core/classes/Color.php");
-	include('core/filters.php');
-	include('core/classes/CommentElement.php');
-	$postElement = new PostElement;
-	
-	if($postElement->query(PostElement::filterId($_GET["id"]))==0)
-		header("location:index.php");
-	$indexElement = new IndexElement;
-	$indexElement->query($postElement->indexId);
-	$destination = $indexElement->destId;
-	$index_id = $indexElement->id;
-	$post_id = $postElement->id;		
-	include('preprocess.php');
-	include('header.php');	
-	include('destination.php');
+include('core/common.php');
+include('core/init.php');
+include('core/classes.php');
+include('core/session.php');
+//include("core/classes/Color.php");
+include('core/filters.php');
+include('core/classes/CommentElement.php');
+$postElement = new PostElement;
 
-	$q = new Db;	
-	$user_info = new User;
+if($postElement->query(PostElement::filterId($_GET["id"]))==0)
+	header("location:index.php");
+$indexElement = new IndexElement;
+$indexElement->query($postElement->indexId);
+$destination = $indexElement->destId;
+$index_id = $indexElement->id;
+$post_id = $postElement->id;		
+include('preprocess.php');
+include('header.php');	
+include('destination.php');
+
+$q = new Db;	
+$user_info = new User;
 //change_template();
 ?>
 <script type="text/javascript" src="js/tiny_mce/tiny_mce.js"></script>
@@ -52,5 +52,4 @@ include('dialog.php');
 include("forms/register_email.php");
 include("footer.php");
 ?>
-
 
