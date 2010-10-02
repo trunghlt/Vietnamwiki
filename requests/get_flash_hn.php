beb3976534c4e8e4dc2cd88b1ec4c151db69aed1
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-02 18:22:57 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@301 46499d64-588c-4cb2-b276-432f5e434212

---
+++ requests/get_flash_hn.php
@@ -0,0 +1,37 @@
+<?php
+function getdata($url, $start, $stop, $str_to_replace='', $str_replace='', $extra_data='',$str_to_replace1='', $str_replace1='') 
+{
+$fd = ""; 
+$start_pos = $end_pos = 0;
+$url = fopen($url, "r");
+while(true) {
+if($end_pos > $start_pos) {
+$result = substr($fd, $start_pos, $end_pos-$start_pos);
+$result .= $stop;
+break;
+}
+$data = fread($url, 8192);
+
+if(strlen($data) == 0) break;
+$fd .= $data;
+if(!$start_pos) $start_pos = strpos($fd, $start);
+if($start_pos) $end_pos = strpos(substr($fd, $start_pos), $stop) + $start_pos;
+}
+fclose($url);
+
+	$info=str_replace($str_to_replace, $str_replace, $extra_data.$result);
+	$info=str_replace($str_to_replace1, $str_replace1, $info);
+	$info=str_replace('src="', 'src="http://hanoi1000.vn/', $info);
+	$info=str_replace('krpano.swf', 'http://hanoi1000.vn/krpano.swf', $info);
+	$info=str_replace('gigapixel.xml', 'http://hanoi1000.vn/gigapixel.xml', $info);
+return $info;
+} 
+?>
+<?php
+		$url="http://hanoi1000.vn/";
+		$start='<body>';
+		$end='<div id="footer">';
+		echo getdata($url,$start,$end,'','','','','');
+		echo '</div>';	
+		echo '</div>';		
+?>
