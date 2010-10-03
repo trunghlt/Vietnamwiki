21380d4ecc0763fcaf20f79232e6603105b5c759
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-03 11:37:16 +0000

    Added pic_banner.php
    git-svn-id: svn://109.123.66.160/home/vnwkrepos@303 46499d64-588c-4cb2-b276-432f5e434212

---
+++ trunk/pic_banner.php
@@ -0,0 +1,3 @@
+<div style="line-height:0px; padding-bottom:1px;">
+<a href="/Ha_Noi-Upcoming-1000th_anniversary_of_Thang_LongHanoi-P89"><img width="100%" height="auto" src="/images/HaNoi1000nam.jpg"/></a>
+</div>
