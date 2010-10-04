7e80dd48171e5e152ba595d5d179dd0cdedb59b3
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-04 17:19:40 +0000

    Added link to the banner
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@313 46499d64-588c-4cb2-b276-432f5e434212

--- pic_banner.php
+++ pic_banner.php
@@ -1,1 +1,1 @@
-<div style="line-height:0px; padding-bottom:1px;"><img width="100%" height="auto" src="/images/HaNoi1000nam.jpg"/></div>
+<a href="/Ha_Noi-Upcoming-1000th_anniversary_of_Thang_LongHanoi-P89"><div style="line-height:0px; padding-bottom:1px;"><img width="100%" height="auto" src="/images/HaNoi1000nam.jpg"/></div></a>
