33cff7deca98b8dd33998513d50249c82db6148e
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-13 04:23:50 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@325 46499d64-588c-4cb2-b276-432f5e434212

--- pic_banner.php
+++ pic_banner.php
@@ -1,2 +1,2 @@
 <!--<a href="/Ha_Noi-Upcoming-1000th_anniversary_of_Thang_LongHanoi-P89"><div style="line-height:0px; padding-bottom:1px;"><img width="100%" height="auto" src="/images/HaNoi1000nam.jpg"/></div></a>-->
-<a href="/Ha_Noi-Upcoming-1000th_anniversary_of_Thang_LongHanoi-P89"><div style="line-height:0px; padding-bottom:1px;"><img width="100%" height="300px" src="/images/roinuoc2.jpg"/></div></a>
+<a href="/Ha_Noi-Upcoming-1000th_anniversary_of_Thang_LongHanoi-P89"><div style="line-height:0px; padding-bottom:1px;"><img width="1343px" height="300px" src="/images/roinuoc2.jpg"/></div></a>
