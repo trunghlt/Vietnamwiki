254eb0aa120c6627364b713ff54b965b3d0e2b6e
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-19 16:37:25 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@336 46499d64-588c-4cb2-b276-432f5e434212

---
+++ upload2/watermark.php
@@ -0,0 +1,30 @@
+<?php
+function watermark($des,$scr,$namefile){
+		header("Content-type: image/jpeg");
+		$imagesource = $des; 
+	
+		$filetype = strtolower(substr($imagesource,strlen($imagesource)-4,4));
+		if($filetype == ".gif") $image = @imagecreatefromgif($imagesource);
+		if($filetype == ".jpg" || $filetype == "jpeg") $image = @imagecreatefromjpeg($imagesource);
+		if($filetype == ".png") $image = @imagecreatefrompng($imagesource);
+		
+		$watermark = @imagecreatefrompng($scr); // file watermark
+		list($watermark_width, $watermark_height) = getimagesize($scr);
+		
+		$watermark_p = imagecreatetruecolor(190, 40);
+		imagecopyresampled($watermark_p, $watermark, 0, 0, 0, 0, 190, 40, $watermark_width, $watermark_height);
+		$watermarkp_width = imagesx($watermark_p);  
+		$watermarkp_height = imagesy($watermark_p);
+		
+		$size = getimagesize($imagesource);
+		$dest_x = $size[0] - $watermarkp_width - 5;  
+		$dest_y = $size[1] - $watermarkp_height - 5;   
+		
+		if(imagecopymerge($image, $watermark_p, $dest_x, $dest_y, 0, 0, $watermarkp_width, $watermarkp_height, 90)){
+			imagejpeg($image,$namefile);
+			imagedestroy($image);
+			imagedestroy($watermark);
+			imagedestroy($watermark_p);
+		}
+}		
+?>
