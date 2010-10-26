9aed0abc4b363185b3d56637a6815a3fe62146c4
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-26 13:15:28 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@353 46499d64-588c-4cb2-b276-432f5e434212

--- viewtopic.php
+++ viewtopic.php
@@ -1,9 +1,59 @@
-<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="100%" height="100%" id="baner" align=center">
-						<param name="allowScriptAccess" value="sameDomain" />
-						<param name="allowFullScreen" value="true" />
-						<param name="movie" value="images/index.swf" /><param name="quality" value="high" />
-						<param name="bgcolor" value="#ffffff" />
-						<param name="wmode" value="transparent">
-						<param name="menu" value="false">
-						<embed src="images/index.swf" quality="high" bgcolor="#ffffff" width="100%" height="100%" name="baner" align="center" allowScriptAccess="sameDomain" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
-</object>
+<?php
+include('core/common.php');
+include('core/init.php');
+include('core/classes.php');
+include('core/session.php');
+//include("core/classes/Color.php");
+include('core/filters.php');
+include('core/classes/CommentElement.php');
+$postElement = new PostElement;
+
+if($postElement->query(PostElement::filterId($_GET["id"]))==0)
+	header("location:index.php");
+$indexElement = new IndexElement;
+$indexElement->query($postElement->indexId);
+$destination = $indexElement->destId;
+$index_id = $indexElement->id;
+$post_id = $postElement->id;		
+include('preprocess.php');
+include('header.php');	
+include('destination.php');
+
+$q = new Db;	
+$user_info = new User;
+//change_template();
+?>
+<script type="text/javascript" src="js/tiny_mce/tiny_mce.js"></script>
+    <td class="center">			
+		<div id="menuWrapper">
+			<?php
+			echo getMainMenu(0, $post_id);
+			?>
+			<div id="toolbar"><?php getToolbarHTML();?></div>
+		</div>
+		<div id="contentTable">			
+			<?php include("viewtopicBody.php") ?>
+		</div>
+	</td>
+     <td classs="right">
+        <?php include('listquestion.php');?>
+    </td></tr>
+<tr>
+	<td colspan=3>
+		<?php include("footLinks.php");?>
+	</td>
+</tr>
+</tbody>
+</table>
+<?php 
+include("forms/editForm.php");
+include("forms/commentForm.php");
+include("forms/composeForm.php");
+include("forms/edit_login.php");
+include("forms/deleteConfirmForm.php");
+include("forms/loginForm.php");
+include('dialog.php');
+include("forms/register_email.php");
+include("footer.php");
+?>
+
