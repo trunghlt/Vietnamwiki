2f73040e6b6a1989ce9e2f255f74eeefd07626a3
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-26 13:04:16 +0000

    git-svn-id: svn://localhost/home/vnwkrepos/trunk@349 46499d64-588c-4cb2-b276-432f5e434212

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" width="100%" height="100%" id="baner" align=center">
 						<param name="allowScriptAccess" value="sameDomain" />
 						<param name="allowFullScreen" value="true" />
-						<param name="movie" value="graphics/index.swf" /><param name="quality" value="high" />
+						<param name="movie" value="images/index.swf" /><param name="quality" value="high" />
 						<param name="bgcolor" value="#ffffff" />
 						<param name="wmode" value="transparent">
 						<param name="menu" value="false">
-						<embed src="graphics/index.swf" quality="high" bgcolor="#ffffff" width="100%" height="100%" name="baner" align="center" allowScriptAccess="sameDomain" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
+						<embed src="images/index.swf" quality="high" bgcolor="#ffffff" width="100%" height="100%" name="baner" align="center" allowScriptAccess="sameDomain" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" />
 </object>
