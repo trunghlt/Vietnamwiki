4602943595aa4f41f8f688b6cff4656860da568c
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2010-10-27 19:17:51 +0000

    Changed common.php so that it uses production constants
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@357 46499d64-588c-4cb2-b276-432f5e434212

--- core/common.php
+++ core/common.php
@@ -18,12 +18,10 @@ define('TEMP_UPLOAD_FOLDER', 'upload2/upload/');
 define('COL', 3);
 define('ROW', 3);
 
-$MEMCACHED_PORT = 11211;
-$GOOGLE_API_KEY = "ABQIAAAAV1hMY6P-vcrStESIcmxsyBSg0YMtASE5KdM7LALqADHM9SZ_PBTZqozQ8fKlIDHry-cBnAxWYeYpSw"; 
-
 include('permalink.php');
 include("APIs.php");
 include('fbconnect.php');
+include("production.php");
 
 #Initialise memcached
 $memcache = new Memcache;
