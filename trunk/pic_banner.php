1443038968d683f72d65167061a4bdc30b6926f3
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-11-08 17:23:29 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos@363 46499d64-588c-4cb2-b276-432f5e434212

--- trunk/pic_banner.php
+++ trunk/pic_banner.php
@@ -1,1 +1,1 @@
-<a href="http://vietnamwiki.net/Ha_Noi-Overview-Hanoi_Capital-P29"><div style="line-height:0px; padding-bottom:1px;"><img width="1000px" height="100%" src="/images/roinuoc2.jpg"/></div></a>
+<a href="http://www.vietnamwiki.net/Vietnam_general-Tradition__culture-Water_puppetry-P238"><div style="line-height:0px; padding-bottom:1px;"><img width="1000px" height="100%" src="/images/roinuoc2.jpg"/></div></a>
