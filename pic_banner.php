999aa2aec674ba529924479d5237697acf99f32e
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-11-23 15:13:41 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@374 46499d64-588c-4cb2-b276-432f5e434212

--- pic_banner.php
+++ pic_banner.php
@@ -1,7 +1,7 @@
     <div style="line-height:0px; padding-bottom:1px;" id="image_text">
         <a href="http://www.vietnamwiki.net/Vietnam_general-Tradition__culture-Water_puppetry-P238">
-        <!--[if IE]> <img style="width:1000px; height:300px;" src="/images/roinuoc2.jpg"/> <![endif]-->
-        <!--[if !IE]>--><img style="width: 1000px; height:300px;" src="/images/roinuoc2.jpg"/> <!--<![endif]-->
+        <!--[if IE]> <img style="width:100%; height:300px;" src="/images/roinuoc2.jpg"/> <![endif]-->
+        <!--[if !IE]>--><img style="width: 100%; height:300px;" src="/images/roinuoc2.jpg"/> <!--<![endif]-->
         </a>
        <div id="text_notification"></div>
     </div>
