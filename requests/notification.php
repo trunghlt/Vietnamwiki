b643e98907fa420a38614a16488f019fa7e1624e
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2010-11-23 14:46:56 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@372 46499d64-588c-4cb2-b276-432f5e434212

---
+++ requests/notification.php
@@ -0,0 +1,39 @@
+<?php
+include("../core/init.php");
+include("../core/common.php");
+include("../core/classes/Db.php");
+include("../core/classes/ActiveRecord.php");
+include("../core/classes/Notification.php");
+if(!logged_in() && Notification::Checkip(myip())==0){
+?>
+   <div>
+     Do you know that VietnamWiki is an open encyclopedia, meaning that you can create and edit articles yourself ? <span class="click_notification" onclick="agree_notification();">Click here</span>
+   </div>
+<style>
+    #image_text #text_notification {
+	position:absolute;
+	top:20px; /* in conjunction with left property, decides the text position */
+	left:100px;
+	width:799px; /* optional, though better have one */
+        height:64px;
+        text-align: center;
+        line-height: 64px;
+        text-decoration: none;
+        color: BLACK;
+        background: #FFC90E;
+        border-radius:50px;
+	-moz-border-radius-bottomleft:50px;
+	-moz-border-radius-bottomright:50px;
+	-moz-border-radius-topleft:50px;
+	-moz-border-radius-topright:50px;
+        -webkit-border-radius:50px;
+    }
+    .click_notification{
+        cursor: pointer;
+        text-decoration: underline;
+        color: blue;
+    }
+</style>
+<?php
+}
+?>
