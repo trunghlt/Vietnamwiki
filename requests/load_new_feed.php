c787ba2173bbbf216abac7bf8f05d933a6d37dd8
hieu <hieu@46499d64-588c-4cb2-b276-432f5e434212>
2011-01-07 06:48:06 +0000

    git-svn-id: svn://109.123.66.160/home/vnwkrepos/trunk@410 46499d64-588c-4cb2-b276-432f5e434212

--- requests/load_new_feed.php
+++ requests/load_new_feed.php
@@ -21,68 +21,81 @@ include("../core/classes/ImageElement.php");
         $time = $_POST["time"];
         $post = new PostElement;
         $des = new DestinationElement;
+        $row_per_page = 5;
+        $num_item = 0;
+        if(is_numeric($_POST["s"]))
+            $s = $_POST["s"];
+        else
+            $s = 0;
+        
         switch($feed_type){
-            case 1 :{
-                    if(is_array($arr = PostElement::getByTime($time))){
-                        echo "<ul>";
-                        foreach($arr as $v){
-                            $str = 'http://www.vietnamwiki.net'.getPostPermalink($v["post_id"]);
-                            $post->query($v["post_id"]);
-                            echo "<li><a href='$str'>$post->title</a></li>";
-                        }
-                        echo "</ul>";
-                    }
-                    else{
-                        echo "No new data";
-                    }
-            };break;
-            case 2 :{
-                    if(is_array($arr = CommentElement::getByTime($time))){
-                        echo "<ul>";
-                        foreach($arr as $v){
-                            if($v["post_id"]!=0){
+            case 1:$arr = PostElement::getByTime($time);$p = "new_articles" ;break;
+            case 2:$arr = CommentElement::getByTime($time);$p = "new_comments" ;break;
+            case 3:$arr = Review::getByTime($time);$p = "new_reviews" ;break;
+            case 4:$arr = ImageElement::getByTime($time);$p = "new_uploaded_images" ;break;
+        }
+        if(is_array($arr)){
+            //////pagination(num page)/////
+            $n_row = count($arr);
+            if($n_row > $row_per_page)
+                $num_page = ceil($n_row/$row_per_page);
+            else 
+                $num_page = 1;
+            $per = (($s/$row_per_page)+1)*$row_per_page;
+            if($per <= $n_row)
+		$r = $per;
+            else
+		$r = $n_row;
+            //////////////////////////////
+
+            echo "<ul>";
+            foreach($arr as $key=>$v){
+                if($num_item == $row_per_page)
+                    break;
+                if($key >= $s && $key < $r ){
+                    $num_item = $num_item + 1;
+                    if($feed_type==1 || $feed_type==2 || $feed_type==3){
+                        if($v["post_id"]!=0){
+                            if($feed_type==1 || $feed_type==2)
                                 $str = 'http://www.vietnamwiki.net'.getPostPermalink($v["post_id"]);
-                                $post->query($v["post_id"]);
-                                echo "<li><a href='$str'>$post->title</a></li>";
-                            }
-                            else{
-                                 echo "<li>No data</li>";
-                            }
-                        }
-                        echo "</ul>";
-                    }
-                    else{
-                        echo "No new data";
-                    }
-            };break;
-            case 3 :{
-                    if(is_array($arr = Review::getByTime($time))){
-                        echo "<ul>";
-                        foreach($arr as $v){
-                            $str = 'http://www.vietnamwiki.net/review.php?&id='.$v["post_id"];
+                            else
+                                $str = 'http://www.vietnamwiki.net/review.php?&id='.$v["post_id"];
+
                             $post->query($v["post_id"]);
-                            echo "<li><a href='$str'>$post->title</a></li>";
+                            if($post->title=='' || $post->title==NULL)
+                                $title = "No title";
+                            else
+                                $title = htmlspecialchars_decode($post->title, ENT_QUOTES);
                         }
-                        echo "</ul>";
+                        else
+                            $title = "no data";
                     }
-                    else{
-                        echo "No new data";
+                    else if($feed_type==4){
+                        $str = 'http://www.vietnamwiki.net/photo.php?dest_id='.$v["dest_id"].'&page=1';
+                        $des->query($v["dest_id"]);
+                        $title = $des->engName;
                     }
-            };break;
-            case 4 :{
-                    if(is_array($arr = ImageElement::getByTime($time))){
-                        echo "<ul>";
-                        foreach($arr as $v){
-                            $str = 'http://www.vietnamwiki.net/photo.php?dest_id='.$v["dest_id"].'&page=1';
-                            $des->query($v["dest_id"]);
-                            echo "<li><a href='$str'>$des->engName</a></li>";
-                        }
-                        echo "</ul>";
+                    echo "<li><a href='$str'>".$title."</a></li>";
+                }
+            }
+            echo "</ul>";
+
+            //////pagionation/////
+            echo "<div class='phantrang'><ul >";
+            if($num_page > 1){
+                $tranghh = ($s/$row_per_page)+1;
+                if($s>0){
+                ?>
+                    <li><a onclick="loadPage(<?php echo $s-$row_per_page;?>,<?=$feed_type?>,<?=$time?>,'<?=$p?>');return false;" style="color:#CC0000; cursor: pointer;">&laquo; Prev</a>&nbsp;&nbsp;&nbsp;</li>
+                <?php
+                }
+                if($s+$row_per_page < $n_row) {?>
+                        <li><a onclick="loadPage(<?php echo $s+$row_per_page;?>,<?=$feed_type?>,<?=$time?>,'<?=$p?>');return false;" style="color:#CC0000; cursor: pointer;">Next &raquo;</a></li>
+                <?php
                     }
-                    else{
-                        echo "No new data";
-                    }
-            };break;
+            }
+            echo "</ul></div>";
+            //////////////////////
         }
     }
     else{
