506011e2c16e0ffe7a23908a4730d6577223e34b
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2011-03-23 01:53:43 +0000

    Fixed founder's call for contribution
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@436 46499d64-588c-4cb2-b276-432f5e434212

--- pic_banner.php
+++ pic_banner.php
@@ -1,7 +1,7 @@
     <div class="founder_words">
         <b>VietnamWiki.net</b> is born out of a simple idea: to provide useful local insight into travelling in Vietnam. Our non-commercial website aims to provide a platform for open user-contributed information and to foster a vibrant community of people from all over the world connected by one same passion for the beauty of Vietnam. Click <a href="/about">here</a> to read more about us.
         <p></p>
-        <b>To editors:</b> Have you ever been embarrassed by being asked "where is Vietnam?" or "how is Vietnam war going?". Vietnam needs your help to build a worldwide reputation of its peace and beauty. <a href="/signup.php">Sign up</a> with VietnamWiki then you can easily share your travel experience in Vietnam to worldwide travellers easily.
+        <b>To editors:</b> Have you ever been embarrassed by being asked "where is Vietnam?" or "how is Vietnam war going?". Vietnam needs your help to build worldwide reputation of its peace and beauty. <a href="/signup.php">Sign up</a> with VietnamWiki then you can easily share your travel experience in Vietnam to worldwide travellers easily. <a href="mailto:vietnamwiki@gmail.com">Email us</a> directly if you want to be a culture leader in your city or region. 
         <p></p>
         <b>To developers:</b> VietnamWiki.net is also <a target="_blank" href="https://github.com/trunghlt/Vietnamwiki">open-sourced</a> and welcome contribution from developers all over the world, especially developers from Vietnam. Passionate students are also very encouraged to join us. <a href="mailto:trunghlt@gmail.com">Email us</a> if you want to be a part of the team.  
     </div>
