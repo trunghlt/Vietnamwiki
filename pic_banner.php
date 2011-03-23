19407a8c95015a88e736eb819e0f3e3268fc5882
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2011-03-23 01:33:44 +0000

    Added founder's call for contribution
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@435 46499d64-588c-4cb2-b276-432f5e434212

--- pic_banner.php
+++ pic_banner.php
@@ -1,3 +1,11 @@
+    <div class="founder_words">
+        <b>VietnamWiki.net</b> is born out of a simple idea: to provide useful local insight into travelling in Vietnam. Our non-commercial website aims to provide a platform for open user-contributed information and to foster a vibrant community of people from all over the world connected by one same passion for the beauty of Vietnam. Click <a href="/about">here</a> to read more about us.
+        <p></p>
+        <b>To editors:</b> Have you ever been embarrassed by being asked "where is Vietnam?" or "how is Vietnam war going?". Vietnam needs your help to build a worldwide reputation of its peace and beauty. <a href="/signup.php">Sign up</a> with VietnamWiki then you can easily share your travel experience in Vietnam to worldwide travellers easily.
+        <p></p>
+        <b>To developers:</b> VietnamWiki.net is also <a target="_blank" href="https://github.com/trunghlt/Vietnamwiki">open-sourced</a> and welcome contribution from developers all over the world, especially developers from Vietnam. Passionate students are also very encouraged to join us. <a href="mailto:trunghlt@gmail.com">Email us</a> if you want to be a part of the team.  
+    </div>
+
     <div style="line-height:0px; padding-bottom:1px;" id="image_text">
         <a href="http://www.vietnamwiki.net/Vietnam_general-Holiday-Tet_festivities_welcome_spring_integration-P21">
         <!--[if IE]> <img style="width:100%; height:auto;" src="/images/lunar_new_year_Tet.jpg"/> <![endif]-->
