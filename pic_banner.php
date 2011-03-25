123bde5c31a5b3b1aa9dd6cf495208610946b0f5
trung <trung@46499d64-588c-4cb2-b276-432f5e434212>
2011-03-25 15:25:15 +0000

    Changed search content -> summary
    git-svn-id: svn://localhost/home/vnwkrepos/trunk@442 46499d64-588c-4cb2-b276-432f5e434212

--- pic_banner.php
+++ pic_banner.php
@@ -1,15 +1,15 @@
     <div class="founder_words">
-        <b>VietnamWiki.net</b> is born out of a simple idea: to provide useful local insight into travelling in Vietnam. Our non-commercial website aims to provide a platform for open user-contributed information and to foster a vibrant community of people from all over the world connected by one same passion for the beauty of Vietnam. Click <a href="/about">here</a> to read more about us.
+        <b><a href="http://www.facebook.com/vietnamwiki.net" target="_blank">VietnamWiki.net</a></b> is born out of a simple idea: to provide useful local insight into travelling in Vietnam. Our non-commercial website aims to provide a platform for open user-contributed information and to foster a vibrant community of people from all over the world connected by one same passion for the beauty of Vietnam. Click <a href="/about">here</a> to read more about us.
         <p></p>
-        <b>To editors:</b> Have you ever been embarrassed by being asked "where is Vietnam?" or "how is Vietnam war going?". Vietnam needs your help to build worldwide reputation of its peace and beauty. <a href="/signup.php">Sign up</a> with VietnamWiki then you can easily share your travel experience in Vietnam to worldwide travellers easily. <a href="mailto:vietnamwiki@gmail.com">Email us</a> directly if you want to be a culture leader in your city or region. 
+        <b>To editors:</b> Have you ever been embarrassed by being asked "where is Vietnam?" or "how is Vietnam war going?". Vietnam needs your help to build worldwide reputation of its peace and beauty. <a href="/signup.php">Sign up</a> with VietnamWiki then you can share your travel experience in Vietnam to worldwide travellers easily. <a href="mailto:vietnamwiki@gmail.com">Email us</a> directly if you want to be a culture leader in your city or region. 
         <p></p>
         <b>To developers:</b> VietnamWiki.net is also <a target="_blank" href="https://github.com/trunghlt/Vietnamwiki">open-sourced</a> and welcome contribution from developers all over the world, especially developers from Vietnam. Passionate students are also very encouraged to join us. <a href="mailto:trunghlt@gmail.com">Email us</a> if you want to be a part of the team.  
     </div>
 
     <div style="line-height:0px; padding-bottom:1px;" id="image_text">
-        <a href="http://www.vietnamwiki.net/Vietnam_general-Holiday-Tet_festivities_welcome_spring_integration-P21">
-        <!--[if IE]> <img style="width:100%; height:auto;" src="/images/lunar_new_year_Tet.jpg"/> <![endif]-->
-        <!--[if !IE]>--><img style="width: 100%; auto;" src="http://www.vietnamwiki.net/images/lunar_new_year_Tet.jpg"/> <!--<![endif]-->
+        <a href="http://www.vietnamwiki.net/Da_Nang-Upcoming-Fireworks_Contest_To_Light_Up_Da_Nang_In_April_2011-P254">
+        <!--[if IE]> <img style="width:100%; height:auto;" src="/images/banners/Danang_fireworks_competition.jpg"/> <![endif]-->
+        <!--[if !IE]>--><img style="width: 100%; height: auto;" src="http://www.vietnamwiki.net/images/banners/Danang_fireworks_competition.jpg"/> <!--<![endif]-->
         </a>
        <div id="text_notification"></div>
     </div>
